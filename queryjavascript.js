e614eb80a06411366fffb3ceadd83bd60f449c09
afloreshernandez <ariannehernandez_2013@yahoo.com>
2020-04-03 17:14:02 -0500

    DOM Manipulation

---
+++ queryjavascript.js
@@ -0,0 +1,9 @@
+document.querySelector('#content').textContent = 'hey';
+
+const exampleList = document.querySelectorAll('.example');
+console.dir(exampleList);
+
+for (let i = 0; i < exampleList.length; i++){
+    
+    exampleList[i].innerText = 'All';
+}
